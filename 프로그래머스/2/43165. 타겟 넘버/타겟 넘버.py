d8d375d137f8f157bb0dff836c63f4abc978ada8
이성규 <101550112+PocachipMind@users.noreply.github.com>
2025-01-07 07:17:25 +0900

    [level 2] Title: 타겟 넘버, Time: 2645.51 ms, Memory: 234 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,19 @@
-answer = 0
+import itertools
+
 def solution(numbers, target):
-    numlen = len(numbers)-1
+    answer = 0
+    pick = [-1, 1]
+    alls = list(itertools.product(pick,repeat=len(numbers)))
     
-    def dfs(num, now):
-        global answer
-        if now == numlen:
-            if num == target:
-                answer += 1
-            return
-        dfs(num+numbers[now+1],now+1)
-        dfs(num-numbers[now+1],now+1)
-        
-    dfs(numbers[0],0)
+    for i in alls:
+        cur = 0
+        for j in enumerate(i):
+            if j[1] > 0:
+                cur += numbers[j[0]]
+            else:
+                cur -= numbers[j[0]]
+        if cur == target:
+            answer += 1
     
-    dfs(-numbers[0],0)
     
     return answer
