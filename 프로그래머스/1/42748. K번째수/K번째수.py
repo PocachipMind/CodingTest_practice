fc7eefb79cb8ca7cd0589c36fb2ff2a241985375
이성규 <101550112+PocachipMind@users.noreply.github.com>
2025-01-07 06:49:29 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,6 @@
 def solution(array, commands):
     answer = []
-    
-    for i in commands:
-        new = sorted(array[i[0]-1:i[1]])
-        answer.append(new[i[2]-1])
+    for l in commands:
+        i,j,k = l
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
-    
-    
