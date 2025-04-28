8a24beb43f569de4a7de84022c44710787660a4a
이성규 <101550112+PocachipMind@users.noreply.github.com>
2025-04-28 23:43:31 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 16.52 ms, Memory: 7.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
@@ -0,0 +1,14 @@
+# str, n = input().strip().split(' ')
+# n = int(n)
+
+
+
+str, n = input().split()
+
+repeat = int(n)
+
+zero = 0
+
+while zero != repeat:
+    print(str, end="")
+    zero = zero + 1
